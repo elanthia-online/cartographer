5fe87d8d57f2c593411dd288423c166389a9cc7e
Benjamin Clos <benjamin.clos@gmail.com>
2025-02-16 08:30:27 -0700

    chore: drop underscore.js;

--- mapdb/tasks/validate.ts
+++ mapdb/tasks/validate.ts
@@ -1,7 +1,5 @@
 import {Project} from "../project"
 import { Room } from "../room"
-import { RoomValidator } from "../validators/room"
-import * as _ from "underscore"
 import { fromError } from 'zod-validation-error'
 
 export interface ValidateConfig {
